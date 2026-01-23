11728f8533689b414f4277baa701c5689339106e
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-01-23 10:50:12 +0800

    01.23 angid

---
+++ src/app/_components/Continue.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+export const Continue = ({ handleAdd }) => {
+  return (
+    <div>
+      <div className="flex justify-center ">
+        <button
+          onClick={handleAdd}
+          className="bg-[#121315] text-white w-80 rounded-sm mt-21 p-1"
+        >
+          Continue 1/3
+        </button>
+      </div>
+    </div>
+  );
+};
